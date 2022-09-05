e425945840ebfe2755ba882af157ab80840696f2
Samuel Thomas <samuel.thomas93@ntlworld.com>
2022-09-05 17:14:54 +0100

    adding completed exercise files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const app = require('./server.js')
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4070;
 
 app.listen(port, () => {
     console.log(`\n Server is running on http://localhost:${port}\n`);
--- src/router.js
+++ src/router.js
@@ -13,12 +13,41 @@ const mockUser = {
     }
 };
 
+const secret = 'very secure password'
+
 router.post('/login', (req, res) => {
 
+    const {username, password} = req.body
+
+    if(mockUser.username === username && mockUser.password === password) {
+        const user = mockUser.username
+
+        const payload = {username: user}
+    
+        const token = jwt.sign(payload, secret)
+
+        return res.status(201).json(token)
+
+        // console.log("payload", payload)
+    }
+
+    else {
+        return res.status(400).json("incorrect username/password provided")
+    }
 });
 
+
 router.get('/profile', (req, res) => {
-  
+    const token = req.headers.authorization
+
+    // console.log("token", token)
+
+    try {
+        jwt.verify(token, secret);
+        return res.json(mockUser)
+    } catch(err) {
+        return res.json("incorrect username/password")
+    }
 });
 
 
